84db0a81d3a7fc5402437ac854471a47ff53efc0
AbdelrahmanElshimy <abdoelsimy123@gmail.com>
2021-07-18 08:49:22 +0200

    Add project files.

---
+++ Metigator/Program.cs
@@ -0,0 +1,22 @@
+﻿using MiHR;
+using System;
+
+namespace Metigator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            string s1 = "dfsfds";
+            string s2 = s1 + "" + "ddsf";
+
+            string s3 = $"{s1} {s2}";
+
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+
+        }
+    }
+}
---
+++ MiHR/HR.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiHR
+{
+    public class HR
+    {
+        public static void X()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
